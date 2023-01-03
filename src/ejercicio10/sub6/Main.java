ba321d7ff6ca0953141d502dd4fbb06662658b10
SiddharthaGF <pablo.guerra@espoch.edu.ec>
2023-01-03 16:06:53 -0500

    add example 10.6

--- src/ejercicio10/sub6/Main.java
+++ src/ejercicio10/sub6/Main.java
@@ -1,12 +1,30 @@
 package ejercicio10.sub6;
 
+import java.util.Scanner;
+
 /**
- *
+ * Leer una palabra o frase e imprimirla al revés, se debe emplear vectores para la resolución
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static char[] ConvertirCadenaAVector(String cadena) {
+        return cadena.toCharArray();
+    }
 
+    private static void ImprimirInverso(char[] letras) {
+        System.out.print("Palabra/Frase al revés -> ");
+        int n = letras.length;
+        for (int i=n-1; i >= 0; i--) {
+            System.out.print(letras[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese una palabra o frase >> ");
+        String cadena = teclado.nextLine();
+        char[] letras = ConvertirCadenaAVector(cadena);
+        ImprimirInverso(letras);
     }
 
 }
